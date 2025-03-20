4331066f7254a654dd96cf11a268dd9813468e37
HelmerBergstrom <hebe2400@student.miun.se>
2025-03-20 18:29:03 +0100

    Fixar meny, sökfält till "movies"-undersidan. Stilar menyn och lägger in generella stilar i SCSS.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Projekt för kursen DT211G - Frontend-baserad webbutveckling.
-# Av Helmer Bergström
+#### Av Helmer Bergström
--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,1 @@
-"use strict";
 
-const url1 = `http://www.omdbapi.com/?s=${searchTitel}&apikey=cde6c1c5`;
-const url2 = `http://www.omdbapi.com/?s=${searchID}&apikey=cde6c1c5`;
-
-fetch(url1)
-    .then(response => response.json())
-    .then(data => 
-        { console.log(data);
-        })
-        .catch(error => console.error("Fel:", error));
