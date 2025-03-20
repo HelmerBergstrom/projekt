79580666f6e4c6f688e6e04c3c8e2e0161c9893b
HelmerBergstrom <hebe2400@student.miun.se>
2025-03-20 20:22:02 +0100

    Lagrar variabler för api-nyckeln som kommer användas för att visa filmer. Stilar även sökfältet och lagrar variabler för sökfunktioner.

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,11 @@
+"use strict";
+
+"use strict";
+
+const apiKey = "cde6c1c5";
+const searchInput = document.getElementById("search-field");
+const searchButton = document.getElementById("search-button");
+const moviesContainer = document.getElementById("home-page-movies");
+
+
 
