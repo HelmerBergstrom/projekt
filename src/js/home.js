0c1c95a1a07a5a9072b3e94c041ef29f56e26c21
HelmerBergstrom <hebe2400@student.miun.se>
2025-03-24 21:23:22 +0100

    Fixar "poster" till startsidan som jag snart ska animera till något snyggt. Kör på Star Wars-posters till detta då dom är många och snygga.

--- src/js/home.js
+++ src/js/home.js
@@ -1,1 +1,47 @@
 "use strict";
+
+const apiKey = "cde6c1c5";
+
+const homePage = document.getElementById("home-page-posters");
+
+window.addEventListener("DOMContentLoaded", () => {
+    if(window.location.pathname === "/" || window.location.pathname.includes("index.html")) {
+        getData();
+    }
+});
+
+async function getData() {
+    const posterUrl1 = `http://www.omdbapi.com/?s=Star_Wars&apikey=${apiKey}`;
+
+    try {
+        const response = await fetch(posterUrl1)
+        const poster = await response.json();
+
+        if(poster.Response === "True") {
+            displayHomePage(poster.Search);
+        } else {
+            homePage.innerHTML = "<p> Vi ber om ursäkt, här ska det egentligen finnas posters för filmer! </p>";
+        }
+
+    } catch(error) {
+        console.error("Fel: ", error);
+    }
+};
+
+function displayHomePage(poster) {
+
+    homePage.innerHTML = "";
+
+    poster.forEach(post => {
+
+        const posterPicture = post.Poster !== "N/A" ? post.Poster : "placeholder.jpg";
+
+        const posterElement = document.createElement("div");
+        posterElement.classList.add("posterPic");
+        posterElement.innerHTML = `
+        <img src="${posterPicture}" alt="${post.Title}">
+        `
+
+        homePage.appendChild(posterElement)
+    });
+};
